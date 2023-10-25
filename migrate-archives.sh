334d9286c1e1cd70bbce89f289e4b83bf27583ad
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-10-25 14:31:42 +0200

    Some test

---
+++ migrate-archives.sh
@@ -0,0 +1,29 @@
+mkdir -p $TMPDIR/$REPO_SCOPE/modules
+while read -r id ; do
+    read -r download
+    read -r version
+
+    echo "Make dirs"
+    mkdir -p $TMPDIR/$REPO_SCOPE/archives/$id/unzipped
+    
+    echo "Downloading $id"
+    curl -L "$download" --output "$TMPDIR/$REPO_SCOPE/archives/$id/$id.zip"
+
+    echo "Decompress $id"
+    unzip -qq $TMPDIR/$REPO_SCOPE/archives/$id/$id.zip -d $TMPDIR/$REPO_SCOPE/archives/$id/unzipped
+
+    echo "Recompress to a valid module"
+    cd $TMPDIR/$REPO_SCOPE/archives/$id/unzipped/*/
+    zip -r "$TMPDIR/$REPO_SCOPE/modules/$id-$version.zip" *
+    cd ~
+
+    TAG_NAME="${REPO_SCOPE^^}-Releases"
+    echo "Using $TAG_NAME for releases"
+
+    if [ -f "$TMPDIR/$REPO_SCOPE/modules/$id-$version.zip" ]; then
+        hub release create -a "$TMPDIR/$REPO_SCOPE/modules/$id-$version.zip" -m "$TAG_NAME" "$TAG_NAME"
+    else
+        echo "Unable to find $TMPDIR/$REPO_SCOPE/modules/$id-$version.zip"
+    fi
+
+done < <( cat "$REPO_SCOPE.json" | jq -r '.modules[] | .id, .download, .version' )
