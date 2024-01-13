e44076b35aa80c4048e37714e5ab8cd08ae0d2dd
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-01-13 01:13:57 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -181,7 +181,7 @@ def make_module_json(repo: Repository):
         # Handle file to ignore the index process for the current module
         if not (properties.get("noIndex") or properties.get("gr_ignore")):
             # Append to skeleton
-            clone_and_zip(repo.html_url, Path(f"modules/{REPO_SCOPE}/{mod_id}.zip"))
+            clone_and_zip(f"{repo.html_url}.git", Path(f"modules/{REPO_SCOPE}/{mod_id}.zip"))
             meta.get("modules").append(module)
 
 if not REPO_NAME == "":
